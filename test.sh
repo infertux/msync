573b98398415f3c7fcf88b9aafcf68670daadfe4
Cédric Félizard <cedric@felizard.fr>
2023-09-28 04:18:31 +0000

    Fix all shellcheck warnings

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@ set -euxo pipefail
 
 cd $(dirname $0)
 
+shellcheck -o all ./msync
+
 gem install bashcov -N
 
 rm -rf coverage
 
 bashcov -- ./msync --help || true
-bashcov -- ./msync rsync://rsync.cyberbits.eu/ruby/ /tmp/msync/ --dry-run --temporary-directory /tmp
+bashcov --mute -- ./msync rsync://rsync.cyberbits.eu/ruby/ /tmp/msync/ --dry-run --temporary-directory /tmp
 bashcov -- ./msync rsync://rsync.cyberbits.eu/archlinux/ /tmp/msync/ --dry-run --quiet --last-update-url https://mirror.cyberbits.eu/archlinux/lastupdate --last-update-sync lastsync --random-delay 1 --warning-timeout 30 --temporary-directory /tmp
