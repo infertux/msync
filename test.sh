d92c8e156d588ed3e527daa66c00a22309b24d0d
Cédric Félizard <cedric@felizard.fr>
2026-09-25 03:17:48 +0000

    Add support for rsync over SSL/TLS

--- test.sh
+++ test.sh
@@ -4,9 +4,20 @@ set -euxo pipefail
 
 cd $(dirname $0)
 
+# Run Shellcheck first
 shellcheck -o all ./msync
 
+# Remove any previous coverage
 rm -rf coverage
+
+# Test with --help
 bashcov -- ./msync --help || true
+
+# Test happy path
 bashcov --mute -- ./msync rsync://rsync.cyberbits.eu/ruby/ /tmp/msync/ --dry-run --temporary-directory /tmp
+
+# Test with SSL/TLS enabled
+bashcov --mute -- ./msync --ssl rsync://rsync.cyberbits.eu/ruby/ /tmp/msync/ --dry-run --temporary-directory /tmp
+
+# Test with --last-update-url
 bashcov -- ./msync rsync://rsync.cyberbits.eu/archlinux/ /tmp/msync/ --dry-run --quiet --last-update-url https://mirror.cyberbits.eu/archlinux/lastupdate --last-update-sync lastsync --random-delay 1 --warning-timeout 30 --temporary-directory /tmp
